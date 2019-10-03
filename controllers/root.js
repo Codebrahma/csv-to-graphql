505aa9c7fba89954aee7a4058bb1a0c9967043b9
Kameshwaran <kamesh@codebrahma.com>
2019-10-03 15:19:31 +0530

    Handled CSV not found issue

--- controllers/root.js
+++ controllers/root.js
@@ -1,5 +1,8 @@
+const fs = require('fs');
+const isFileExists = require('util').promisify(fs.exists);
+const graphqlHTTP = require('express-graphql');
+
 const CSVProcessor = require('./../services/csvProcessor');
-const graphqlHTTP = require('express-graphql')
 
 class RootController {
   static index(_req, res) {
@@ -8,16 +11,28 @@ class RootController {
 
   static async upload(req, res) {
     if (!req.file || !req.file.path) {
-      return res.redirect('/?notice=CSV File is required to proceed')
+      return res.redirect('/?notice=CSV File is required to proceed');
     }
     res.redirect(`/csv-to-graphql?path=${req.file.path}`);
   }
 
   static async csvToGraphql(req, res) {
-    const schema = await CSVProcessor.process(req.query.path);
+    const {
+      query: { path = '' },
+    } = req;
+
+    if (!path) {
+      return res.redirect('/?notice=CSV File is required to proceed');
+    }
+
+    if (!(await isFileExists(path))) {
+      return res.redirect('/?notice=CSV File not found. Perhaps it could be expired or deleted.');
+    }
+
+    const schema = await CSVProcessor.process(path);
     graphqlHTTP({
       schema,
-      graphiql: true
+      graphiql: true,
     })(req, res);
   }
 }
